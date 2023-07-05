58b76f4b4a621ba537c564e8da1bc5f094af53a3
Jeremy Magland <jmagland@flatironinstitute.org>
2023-07-05 12:45:16 -0400

    raster plot default visible range

--- gui/src/pages/BrowsePage/FileView/SpikeTrainsFileView.tsx
+++ gui/src/pages/BrowsePage/FileView/SpikeTrainsFileView.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent, useEffect, useState } from "react";
+import { useTimeRange } from "../../../package/context-timeseries-selection";
 import { useRtcshare } from "../../../rtcshare/useRtcshare";
 import validateObject, { isArrayOf, isEqualTo, isNumber, isOneOf, isString } from "../../../types/validateObject";
 import RasterPlotView3 from "./RasterPlotView3/RasterPlotView3";
@@ -55,6 +56,16 @@ const SpikeTrainsFileView: FunctionComponent<Props> = ({width, height, filePath}
         })()
         return () => {canceled = true}
     }, [client, filePath])
+
+    // initialize the visible time range
+    const startTimeSec = spikeTrainsClient?.startTimeSec
+    const endTimeSec = spikeTrainsClient?.endTimeSec
+    const { setVisibleTimeRange } = useTimeRange()
+    useEffect(() => {
+        if (startTimeSec === undefined) return
+        if (endTimeSec === undefined) return
+        setVisibleTimeRange(startTimeSec, Math.min(startTimeSec + 60 * 3, endTimeSec))
+    }, [startTimeSec, endTimeSec, setVisibleTimeRange])
 
     if (!spikeTrainsClient) return <div>Loading...</div>
 
