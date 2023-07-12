d33114d60b8784cd488da795d51db38ef241454f
Jeremy Magland <jmagland@flatironinstitute.org>
2023-07-12 13:24:54 -0400

    change logo

--- gui/src/ApplicationBar.tsx
+++ gui/src/ApplicationBar.tsx
@@ -34,10 +34,10 @@ const ApplicationBar: FunctionComponent<Props> = () => {
     // light greenish background color for app bar
     // const barColor = '#e0ffe0'
 
-    const barColor = '#feb'
+    const barColor = '#65a6fc'
 
     // const bannerColor = '#00a000'
-    const titleColor = '#a70'
+    const titleColor = 'white'
     const bannerColor = titleColor
 
     const star = <span style={{color: bannerColor, fontSize: 20}}>★</span>
@@ -47,7 +47,7 @@ const ApplicationBar: FunctionComponent<Props> = () => {
             <AppBar position="static" style={{height: applicationBarHeight - 10, color: 'black', background: barColor}}>
                 <Toolbar style={{minHeight: applicationBarHeight - 10}}>
                     <img src={logoUrl} alt="logo" height={30} style={{paddingBottom: 5, cursor: 'pointer'}} onClick={onHome} />
-                    <div onClick={onHome} style={{cursor: 'pointer', color: titleColor, fontStyle: 'italic'}}>&nbsp;&nbsp;&nbsp;Neurosift</div>
+                    <div onClick={onHome} style={{cursor: 'pointer', color: titleColor}}>&nbsp;&nbsp;&nbsp;Neurosift</div>
                     <div style={{color: bannerColor, position: 'relative', top: -2}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{star} This viewer is in alpha and is under <Hyperlink color={bannerColor} href="https://github.com/flatironinstitute/neurosift" target="_blank">active development</Hyperlink> {star}</div>
                     <span style={{marginLeft: 'auto'}} />
                     {
