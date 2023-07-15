3a6a211802377a0ad8e9ff232f4a78aec5532077
Jeremy Magland <jmagland@flatironinstitute.org>
2023-07-15 11:56:51 -0400

    improve main tab meta info display

--- gui/src/pages/NwbPage/NwbMainView/NwbMainLeftPanel.tsx
+++ gui/src/pages/NwbPage/NwbMainView/NwbMainLeftPanel.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useMemo } from "react"
 import { useNwbOpenTabs } from "../NwbOpenTabsContext"
 import { RemoteH5File } from "../RemoteH5File/RemoteH5File"
 import { useDatasetData, useGroup } from "./NwbMainView"
@@ -10,19 +10,55 @@ type Props = {
     nwbFile: RemoteH5File
 }
 
+const labelMap: {name: string, newName: string}[] = [
+    {name: 'session_id', newName: 'Session ID'},
+    {name: 'experimenter', newName: 'Experimenter'},
+    {name: 'lab', newName: 'Lab'},
+    {name: 'institution', newName: 'Institution'},
+    {name: 'related_publications', newName: 'Related publications'},
+    {name: 'experiment_description', newName: 'Experiment description'},
+    {name: 'session_description', newName: 'Session description'},
+    {name: 'identifier', newName: 'Identifier'},
+    {name: 'session_start_time', newName: 'Session start'},
+    {name: 'timestamps_reference_time', newName: 'Timestamps ref.'},
+    {name: 'file_create_date', newName: 'File creation'},
+]
+
 const NwbMainLeftPanel: FunctionComponent<Props> = ({width, height, nwbFile}) => {
     const rootGroup = useGroup(nwbFile, '/')
-    const {data: fileCreateDateData} = useDatasetData(nwbFile, '/file_create_date')
-    const {data: sessionStartTimeData} = useDatasetData(nwbFile, '/session_start_time')
-    const {data: timestampsReferenceTimeData} = useDatasetData(nwbFile, '/timestamps_reference_time')
-    const {data: sessionDescriptionData} = useDatasetData(nwbFile, '/session_description')
-    const {data: identifierData} = useDatasetData(nwbFile, '/identifier')
-    const {data: experimenterData} = useDatasetData(nwbFile, '/general/experimenter')
-    const {data: institutionData} = useDatasetData(nwbFile, '/general/institution')
-    const {data: labData} = useDatasetData(nwbFile, '/general/lab')
-    const {data: relatedPublicationsData} = useDatasetData(nwbFile, '/general/related_publications')
-    const {data: sessionIdData} = useDatasetData(nwbFile, '/general/session_id')
-    const {openTab} = useNwbOpenTabs()
+    const generalGroup = useGroup(nwbFile, '/general')
+
+    const items = useMemo(() => {
+        const ret: {name: string, path: string}[] = []
+        rootGroup?.datasets.forEach(ds => {
+            const newName = labelMap.find(x => (x.name === ds.name))?.newName || ds.name
+            ret.push({name: newName || ds.name, path: ds.path})
+        })
+        generalGroup?.datasets.forEach(ds => {
+            const newName = labelMap.find(x => (x.name === ds.name))?.newName || ds.name
+            ret.push({name: newName || ds.name, path: ds.path})
+        })
+        return ret
+    }, [rootGroup, generalGroup])
+
+    const itemsSorted = useMemo(() => {
+        const ret = [...items]
+        ret.sort((a, b) => {
+            const ind1 = labelMap.findIndex(x => (x.newName === a.name))
+            const ind2 = labelMap.findIndex(x => (x.newName === b.name))
+            if ((ind1 >= 0)) {
+                if (ind2 < 0) return -1
+                return ind1 - ind2
+            }
+            if ((ind2 >= 0)) {
+                if (ind1 < 0) return 1
+                return ind1 - ind2
+            }
+            return a.name.localeCompare(b.name)
+        })
+        return ret
+    }, [items])
+
     const bottomBarHeight = 27
     return (
         <div className="LeftPanel" style={{position: 'absolute', width, height}}>
@@ -31,50 +67,17 @@ const NwbMainLeftPanel: FunctionComponent<Props> = ({width, height, nwbFile}) =>
                     className="nwb-table"
                 >
                     <tbody>
+                        {
+                            itemsSorted.map(item => (
+                                <tr key={item.name}>
+                                    <td>{item.name}</td>
+                                    <td><DatasetDataView nwbFile={nwbFile} path={item.path} /></td>
+                                </tr>
+                            ))
+                        }
                         <tr>
-                            <td>Session ID</td>
-                            <td>{sessionIdData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Experimenter</td>
-                            <td>{experimenterData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Lab</td>
-                            <td>{labData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Institution</td>
-                            <td>{institutionData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Related publications</td>
-                            <td>{relatedPublicationsData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Description</td>
-                            <td>{sessionDescriptionData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Identifier</td>
-                            <td>{identifierData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Session start</td>
-                            <td>{sessionStartTimeData || ''}</td>
-                        </tr>
-                        <tr>
-                            <td>Timestamps ref.</td>
-                            <td>{timestampsReferenceTimeData || ''}</td>
-                        </tr>
-
-                        <tr>
-                            <td>File creation</td>
-                            <td>{fileCreateDateData ? fileCreateDateData[0] : ''}</td>
-                        </tr>
-                        <tr>
-                            <td>nwb_version</td>
-                            <td>{rootGroup?.attrs['nwb_version']}</td>
+                            <td>NWB version</td>
+                            <td>{rootGroup?.attrs['nwb_version'] || 'Loading...'}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -83,6 +86,17 @@ const NwbMainLeftPanel: FunctionComponent<Props> = ({width, height, nwbFile}) =>
             </div>
         </div>
     )
+}
+
+type DatasetDataViewProps = {
+    nwbFile: RemoteH5File
+    path: string
+}
+
+const DatasetDataView: FunctionComponent<DatasetDataViewProps> = ({nwbFile, path}) => {
+    const {data: datasetData} = useDatasetData(nwbFile, path)
+    if (!datasetData) return <span>Loading...</span>
+    return <span>{datasetData}</span>
 }
 
 export default NwbMainLeftPanel
