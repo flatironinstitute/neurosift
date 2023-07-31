bf7e777955c47590b586122fd7839ec14e7962c4
Jeremy Magland <jmagland@flatironinstitute.org>
2023-07-31 10:52:52 -0400

    sortable columns in dynamic table view

--- gui/src/pages/NwbPage/viewPlugins/DynamicTable/DynamicTableView.tsx
+++ gui/src/pages/NwbPage/viewPlugins/DynamicTable/DynamicTableView.tsx
@@ -28,11 +28,68 @@ const dataReducer = (state: DataState, action: DataAction) => {
     else return state
 }
 
-const DynamicTableView: FunctionComponent<Props> = ({ width, height, path, condensed }) => {
+type ColumnSortState = {
+    primary?: {
+        column: string
+        ascending: boolean
+    }
+    secondary?: {
+        column: string
+        ascending: boolean
+    }
+}
+
+type ColumnSortAction = {
+    type: 'click'
+    column: string
+}
+
+const columnSortReducer = (state: ColumnSortState, action: ColumnSortAction): ColumnSortState => {
+    if (action.type === 'click') {
+        if (state.primary) {
+            // the primary column is already set
+            if (state.primary.column === action.column) {
+                // the primary column is the same as the one that was clicked
+                return {
+                    ...state,
+                    primary: {
+                        ...state.primary,
+                        ascending: !state.primary.ascending // switch the sort order
+                    }
+                }
+            }
+            else {
+                // the primary column becomes the secondary column
+                return {
+                    ...state,
+                    secondary: state.primary,
+                    primary: {
+                        column: action.column,
+                        ascending: true
+                    }
+                }
+            }
+        }
+        else {
+            // the primary column is not set
+            return {
+                ...state,
+                primary: {
+                    column: action.column,
+                    ascending: true
+                }
+            }
+        }
+    }
+    else return state
+}
+
+const DynamicTableView: FunctionComponent<Props> = ({ width, height, path }) => {
     const nwbFile = useContext(NwbFileContext)
     if (!nwbFile) throw Error('Unexpected: nwbFile is null')
     const [data, dataDispatch] = useReducer(dataReducer, {})
     const group = useGroup(nwbFile, path)
+    const [columnSortState, columnSortDispatch] = useReducer(columnSortReducer, {primary: {column: 'id', ascending: true}})
     const colnames = useMemo(() => {
         if (!group) return undefined
         let c = group.attrs['colnames'] as string[]
@@ -91,6 +148,35 @@ const DynamicTableView: FunctionComponent<Props> = ({ width, height, path, conde
         return ret
     }, [colnames, data])
 
+    const sortedRowItems = useMemo(() => {
+        if (!colnames) return rowItems
+        const primary = columnSortState.primary
+        const secondary = columnSortState.secondary
+        if (!primary) return rowItems
+        const primaryColIndex = colnames.indexOf(primary.column)
+        if (primaryColIndex < 0) return rowItems
+        const secondaryColIndex = secondary ? colnames.indexOf(secondary.column) : -1
+        const ret = [...rowItems]
+        ret.sort((a, b) => {
+            const valA = a.columnValues[primaryColIndex]
+            const valB = b.columnValues[primaryColIndex]
+            if (valA === undefined) return 1
+            if (valB === undefined) return -1
+            if (valA < valB) return primary.ascending ? -1 : 1
+            if (valA > valB) return primary.ascending ? 1 : -1
+            if ((secondaryColIndex >= 0) && (secondary)) {
+                const valA2 = a.columnValues[secondaryColIndex]
+                const valB2 = b.columnValues[secondaryColIndex]
+                if (valA2 === undefined) return 1
+                if (valB2 === undefined) return -1
+                if (valA2 < valB2) return secondary.ascending ? -1 : 1
+                if (valA2 > valB2) return secondary.ascending ? 1 : -1
+            }
+            return 0
+        })
+        return ret
+    }, [colnames, rowItems, columnSortState])
+
     if (!colnames) return <div>Loading...</div>
     return (
         <div style={{position: 'absolute', width, height, overflowY: 'auto'}}>
@@ -99,14 +185,26 @@ const DynamicTableView: FunctionComponent<Props> = ({ width, height, path, conde
                     <tr>
                         {
                             colnames.map((colname) => (
-                                <th key={colname}>{colname}</th>
+                                <th key={colname}>
+                                    <span
+                                        onClick={() => {columnSortDispatch({type: 'click', column: colname})}}
+                                        style={{cursor: 'pointer'}}
+                                    >
+                                        {
+                                            (columnSortState.primary) && (columnSortState.primary.column === colname) && (
+                                                <span style={{color: 'white'}}>{columnSortState.primary.ascending ? '▲' : '▼'}&nbsp;</span>
+                                            )
+                                        }
+                                        {colname}
+                                    </span>
+                                </th>
                             ))
                         }
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        rowItems.map((rowItem, i) => (
+                        sortedRowItems.map((rowItem, i) => (
                             <tr key={i}>
                                 {
                                     rowItem.columnValues.map((val, j) => (
