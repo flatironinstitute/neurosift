c68b0ff3680ca896fc8d934f4823a4d433610a5f
Jeremy Magland <jmagland@flatironinstitute.org>
2024-10-04 09:32:45 -0400

    pynapple chat context
    Create file for 2D tuning curves with Pynapple

---
+++ doc/ai_assistant_examples.md
@@ -0,0 +1,7 @@
+## Creating 2D tuning curves with Pynapple
+
+Demonstrates the consultation of additional resources.
+
+https://neurosift.app/?p=/nwb&url=https://api.dandiarchive.org/api/assets/ffcb1836-587e-42f4-887b-50b02948b779/download/&dandisetId=000623&dandisetVersion=0.240227.2023
+"Show how to create 2D tuning curves for this session including how to load this NWB file."
+
--- gui/src/app/pages/NwbPage/NwbPage.tsx
+++ gui/src/app/pages/NwbPage/NwbPage.tsx
@@ -745,6 +745,29 @@ This will be a Ts object if the timeseries is 1D, a TsdFrame object if it is 2D,
 Similarly, NWB AnnotationsSeries objects can be loaded as Pynapple Ts objects, and NWB TimeIntervals objects can be loaded as Pynapple IntervalSet objects.
 `,
   },
+  {
+    fileName: "using_pynapple_to_create_2d_tuning_curves.md",
+    description: "Using Pynapple to create 2D tuning curves",
+    content: `
+Here's an example of how to create 2D tuning curves using Pynapple:
+\`\`\`python
+import pynapple as nap
+# ... get the pynwb file object 'nwbfile' ...
+nwbp = nap.NWBFile(nwbfile)
+units = nwbp["units"]
+position = nwbp["position"]
+tc, binsxy = nap.compute_2d_tuning_curves(units, position, 20)
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(15, 7))
+for i in tc.keys():
+    plt.subplot(2, 4, i + 1)
+    plt.imshow(tc[i], origin="lower", aspect="auto")
+    plt.title("Unit {}".format(i))
+plt.tight_layout()
+plt.show()
+\`\`\``,
+  },
 ];
 
 // Having trouble with getting chatbot to understand pynapple. This was my attempt but it didn't really work.
