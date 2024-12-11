987b18443c1b116138d75961c9da6c0c2cf7dbe1
Jeremy Magland <jmagland@flatironinstitute.org>
2024-12-11 12:59:13 -0500

    re-enable lindi for dandisets

--- gui/src/app/pages/NwbPage/NwbPage.tsx
+++ gui/src/app/pages/NwbPage/NwbPage.tsx
@@ -576,10 +576,10 @@ const getFileDownloadUrlNewKachery = async (
 };
 
 export const tryGetLindiUrl = async (url: string, dandisetId: string) => {
-  // disable these for now because retrieval of lindi not working from Brown u.
-  if (["000248", "001195"].includes(dandisetId)) {
-    return undefined;
-  }
+  // // disable these for now because retrieval of lindi not working from Brown u.
+  // if (["000248", "001195"].includes(dandisetId)) {
+  //   return undefined;
+  // }
 
   if (url.endsWith(".lindi.json") || url.endsWith(".lindi.tar")) {
     return url;
