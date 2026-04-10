288921d38d15174326e9c757c6b2abd8ee8c7fc1
Heberto Mayorquin <h.mayorquin@gmail.com>
2026-04-09 19:53:27 -0600

    test prettier

--- src/pages/VideoPage/VideoViewer.tsx
+++ src/pages/VideoPage/VideoViewer.tsx
@@ -87,8 +87,26 @@ const VideoViewer: FunctionComponent<Props> = ({ videoUrl, height }) => {
       }}
     >
       {playbackError && (
-        <div style={{ marginBottom: "12px", color: "red" }}>
-          {playbackError}
+        <div
+          style={{
+            marginBottom: "12px",
+            padding: "18px 20px",
+            border: "1px solid #f1b5b5",
+            borderRadius: 8,
+            background: "#fff5f5",
+            color: "#8a1c1c",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 18,
+              fontWeight: 600,
+              marginBottom: 8,
+            }}
+          >
+            Video Not Supported
+          </div>
+          <div style={{ lineHeight: 1.5 }}>{playbackError}</div>
         </div>
       )}
       {!playbackError && (
